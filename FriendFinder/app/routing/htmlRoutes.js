282ec1f85e0761f76eabab279f8297e2ee3b0e13
Valarie Cardoza <vjcardoza2011@yahoo.com>
2019-06-08 16:20:27 -0500

    updated both routes files

---
+++ FriendFinder/app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+module.exports = function(app, path) {
+  // A default, catch-all route that leads to `home.html` which displays the home page.
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  // A GET Route to `/survey` that displays the survey page.
+  app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+};
